669f33774a78d52ce7a64f22e6f0b685383b6498
krauserrobledo <krauser.robledo@gmail.com>
2025-10-16 12:14:57 +0200

    feat(repository): Repository interfaces implementation

---
+++ Application/Abstractions/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace Application.Abstractions
+{
+    public interface IUserRepository
+    {
+        // Basic CRUD operations
+        Task<User?> GetById(string userId);
+        Task<User?> GetByEmail(string email);
+        Task<bool> Exists(string userId);
+        Task<User> Create(User user);
+
+        // Additional methods for user-related operations
+        Task<User> GetUserWithTasks(string userId);
+        Task<int> GetUserTaskCount(string userId);
+
+
+    }
+}
---
+++ MinimalApi/Program.cs
@@ -0,0 +1,49 @@
+using Application.Abstractions;
+using Data.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ITagRepository, TagRepository>();
+builder.Services.AddScoped<ISubTaskRepository, SubTaskRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
