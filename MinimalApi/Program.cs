9045d2927813d22cff99e6bca044f61adc16cd14
krauserrobledo <krauser.robledo@gmail.com>
2025-10-16 17:33:19 +0200

    feat(identity): Identity implementation

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,12 +1,18 @@
 using Application.Abstractions;
 using Data;
 <<<<<<< HEAD
+<<<<<<< HEAD
 using Data.Identity;
 using Data.Repositories;
 using Microsoft.AspNetCore.Identity;
 =======
 using Data.Repositories;
 >>>>>>> 482bc95 (fix(data): Bug fix in data layer before Identity implementation)
+=======
+using Data.Identity;
+using Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+>>>>>>> af90a08 (feat(identity): Identity implementation)
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,22 +25,31 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString));
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> af90a08 (feat(identity): Identity implementation)
 // Identity DI
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
+<<<<<<< HEAD
 =======
 >>>>>>> 482bc95 (fix(data): Bug fix in data layer before Identity implementation)
+=======
+>>>>>>> af90a08 (feat(identity): Identity implementation)
 // Add repositories
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ITagRepository, TagRepository>();
 builder.Services.AddScoped<ISubtaskRepository, SubtaskRepository>();
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 >>>>>>> 482bc95 (fix(data): Bug fix in data layer before Identity implementation)
+=======
+>>>>>>> af90a08 (feat(identity): Identity implementation)
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
