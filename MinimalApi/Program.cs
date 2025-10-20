43d76d8455174edd7c294da3188443444c4ad5e0
krauserrobledo <krauser.robledo@gmail.com>
2025-10-20 20:59:33 +0200

    feat(middleware): Middleware implementation

---
+++ MinimalApi/Program.cs
@@ -0,0 +1,104 @@
+using Domain.Abstractions;
+using Data.Services;
+using Data;
+using Data.Identity;
+using Data.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MinimalApi.Endpoints;
+using System.Text;
+using Data.Abstractions;
+using MinimalApi.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Add DbContext with SQL Server provider
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Identity DI
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequiredLength = 6;
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+// Repositories DI
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ITagRepository, TagRepository>();
+builder.Services.AddScoped<ISubtaskRepository, SubtaskRepository>();
+
+
+// Minimal API Services DI
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// ✅ CONFIGURATION JWT
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var key = Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]!);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidateAudience = true,
+        ValidAudience = jwtSettings["Audience"],
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddAuthorization();
+
+// ✅ DI - Token Service
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+// Custom Middlewares
+app.UseExceptionHandlingMiddleware();
+app.UseRequestLogginMiddleware();
+// HTTPS Middleware
+app.UseHttpsRedirection();
+// JWT Middleware
+app.UseAuthentication();
+// Auth Middleware
+app.UseAuthorization();
+
+
+
+// Map Endpoints
+app.MapAuthEndpoints();
+app.MapTaskEndpoints();
+app.MapTagEndpoints();
+app.MapCategoryEndpoints();
+app.MapSubtaskEndpoints();
+
+
+app.Run();
+
